9d2fc49a58c2a067df80b6f081726dfe4f85153d
danielkull <danielkull117@gmail.com>
2023-05-27 14:37:42 +0200

    fix: solves issue, splits compontens adds button per row

--- task/my-first-vue-cli-project/src/components/BookList.vue
+++ task/my-first-vue-cli-project/src/components/BookList.vue
@@ -6,23 +6,25 @@
         <tr>
           <th class="table-item__table-head-name">Name</th>
           <th class="table-item__table-head--isbn">ISBN</th>
+          <th class="table-item__table-head-actions">Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr
+        <BookListRow
           v-for="book in books"
           :key="book.isbn"
+          :title="book.title"
+          :isbn="book.isbn"
           class="table-item__table-row"
-        >
-          <td>{{ book.title }}</td>
-          <td>{{ book.isbn }}</td>
-        </tr>
+        />
       </tbody>
     </table>
   </section>
 </template>
 
 <script>
+import BookListRow from "@/components/BookListRow.vue";
+
 const state = {
   books: [
     {
@@ -61,6 +63,9 @@ const state = {
 };
 
 export default {
+  components: {
+    BookListRow,
+  },
   data() {
     return {
       books: state.books,
@@ -102,8 +107,7 @@ export default {
   color: #ffffff;
   text-align: left;
 }
-.table-item__table th,
-.table-item__table td {
+.table-item__table th {
   padding: 12px 15px;
 }
 .table-item__table tbody tr {
---
+++ task/my-first-vue-cli-project/src/components/BookListRow.vue
@@ -0,0 +1,28 @@
+<template>
+  <tr class="table-item__table-row">
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+    <td><BaseButton text="Add Book" variant="primary" /></td>
+  </tr>
+</template>
+
+<script>
+import BaseButton from "@/components/BaseButton.vue";
+
+export default {
+  components: {
+    BaseButton,
+  },
+  name: "BookListRow",
+  props: {
+    title: String,
+    isbn: String,
+  },
+};
+</script>
+
+<style scoped>
+.table-item__table td {
+  padding: 12px 15px;
+}
+</style>
