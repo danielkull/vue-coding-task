7611ce664eb5811e342b30b6e56a425de4b3a5d2
danielkull <danielkull117@gmail.com>
2023-05-27 19:34:30 +0200

    feat: create attendee-counter app with increase/decrease functionality

---
+++ task/attendee-counter/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <h1>Count all people by their age</h1>
+  <h2>Total count: {{ totalCount }}</h2>
+  <AttendeeCounter
+    v-for="({ text, counter }, index) of attendeeCounters"
+    :key="text"
+    :text="text"
+    :counter="counter"
+    @increase="handleIncrease(index)"
+    @decrease="handleDecrease(index)"
+  />
+</template>
+
+<script>
+import AttendeeCounter from "./components/AttendeeCounter.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      attendeeCounters: [
+        {
+          text: "5-12 years",
+          counter: 0,
+        },
+        {
+          text: "12-18 years",
+          counter: 0,
+        },
+        {
+          text: "18-30 years",
+          counter: 0,
+        },
+        {
+          text: "30-50 years",
+          counter: 0,
+        },
+        {
+          text: "50 > years",
+          counter: 0,
+        },
+      ],
+    };
+  },
+  computed: {
+    totalCount() {
+      let totalCount = 0;
+      this.attendeeCounters.forEach(({ counter }) => (totalCount += counter));
+      return totalCount;
+    },
+  },
+  components: {
+    AttendeeCounter,
+  },
+  methods: {
+    handleIncrease(index) {
+      this.attendeeCounters[index].counter++;
+    },
+    handleDecrease(index) {
+      // console.log(this);
+      this.attendeeCounters[index].counter--;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ task/attendee-counter/src/components/AttendeeCounter.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>{{ text }}: {{ counter }}</p>
+  <button @click="increase">+</button>
+  <button @click="decrease">-</button>
+  <hr />
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    counter: Number,
+  },
+  emits: ["increase", "decrease"],
+  methods: {
+    increase() {
+      this.$emit("increase");
+    },
+    decrease() {
+      if (this.counter > 0) {
+        this.$emit("decrease");
+      }
+    },
+  },
+};
+</script>
