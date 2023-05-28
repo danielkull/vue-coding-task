0df3d5acdbd83dc5bb997ce6d22fe8ea48582bf3
danielkull <danielkull117@gmail.com>
2023-05-28 14:26:53 +0200

    initial commit

---
+++ task/base-list/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <BaseList
+    :headline="fruitList.headline"
+    :intro-text="fruitList.introText"
+    :list-items="fruitList.data"
+  />
+  <BaseList
+    :headline="attendeeList.headline"
+    :intro-text="attendeeList.introText"
+    :list-items="attendeeList.data"
+  />
+</template>
+
+<script>
+import BaseList from "@/components/BaseList.vue";
+
+export default {
+  name: "App",
+  components: {
+    BaseList,
+  },
+  data() {
+    return {
+      fruitList: {
+        headline: "Fruits",
+        introText: "A list of available fruits",
+        data: [
+          {
+            id: 41,
+            text: "Apple",
+            emoji: "🍏",
+          },
+          {
+            id: 22,
+            text: "Melon",
+            emoji: "🍉",
+          },
+          {
+            id: 1,
+            text: "Kiwi",
+            emoji: "🥝",
+          },
+        ],
+      },
+      attendeeList: {
+        headline: "Bootcamp Attendees",
+        introText: "A list of bootcamp attendees in 2022",
+        data: [
+          {
+            id: 41,
+            text: "Werner",
+            age: "42",
+          },
+          {
+            id: 4,
+            text: "Melanie",
+            age: "35",
+          },
+          {
+            id: 1,
+            text: "Charlotte",
+            age: "24",
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ task/base-list/src/components/BaseList.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>{{ headline }}</h2>
+  <div class="intro-text">{{ introText }}</div>
+  <ul>
+    <li v-for="item in listItems" :key="item.id">{{ item.text }}</li>
+  </ul>
+</template>
+
+<script>
+const timestamp = +new Date();
+export default {
+  props: {
+    headline: String,
+    introText: String,
+    listItems: {
+      type: Array,
+      default: () => [{ text: "No items exists", id: timestamp }],
+    },
+  },
+};
+</script>
